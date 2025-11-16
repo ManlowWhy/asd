4e8777644edc0d7a52cf01140a6bd63faf27c1cf
ManlowWhy <olmansr2007@gmail.com>
2025-11-16 05:43:09 -0600

    das
    da

---
+++ ArbolGen.Tests/EdadTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace ArbolGen.Tests
+{
+    [TestClass]
+    public class EdadTests
+    {
+        private static int CalcularEdadPrivado(DateTime nacimiento)
+        {
+            var acceso = new PrivateType(typeof(MapaTest.FormRegistro));
+            return (int)acceso.InvokeStatic("CalcularEdad", new object[] { nacimiento });
+        }
+
+        [TestMethod]
+        public void CalcularEdad_CuandoCumpleaniosYaPaso_EdadExacta()
+        {
+           
+            DateTime nacimiento = DateTime.Today.AddYears(-20).AddDays(-1);
+
+            int edad = CalcularEdadPrivado(nacimiento);
+
+            Assert.AreEqual(20, edad);
+        }
+
+        [TestMethod]
+        public void CalcularEdad_CuandoCumpleaniosEsManana_NoSeCuentaTodavia()
+        {
+            
+            DateTime nacimiento = DateTime.Today.AddYears(-20).AddDays(1);
+
+            int edad = CalcularEdadPrivado(nacimiento);
+
+            Assert.AreEqual(19, edad);
+        }
+
+        [TestMethod]
+        public void CalcularEdad_FechaFutura_RegresaCero()
+        {
+            DateTime nacimiento = DateTime.Today.AddYears(1);
+
+            int edad = CalcularEdadPrivado(nacimiento);
+
+            Assert.AreEqual(0, edad);
+        }
+    }
+}
---
+++ ArbolGen.Tests/RelacionesArbolTests.cs
@@ -0,0 +1,72 @@
+﻿#nullable disable
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using MapaTest;
+
+namespace ArbolGen.Tests
+{
+    [TestClass]
+    public class RelacionesArbolTests
+    {
+      
+        private PrivateObject _registroPrivado;
+
+        [TestInitialize]
+        public void CrearInstanciaSinConstructor()
+        {
+          
+            var instancia = (FormRegistro)FormatterServices
+                .GetUninitializedObject(typeof(FormRegistro));
+
+            _registroPrivado = new PrivateObject(instancia);
+        }
+
+        [TestMethod]
+        public void ObtenerPadresSegunParentezco_Hijo_RegresaMadreYPadre()
+        {
+            var resultado = (List<string>)_registroPrivado!
+                .Invoke("ObtenerPadresSegunParentezco", "Hijo");
+
+            CollectionAssert.AreEqual(
+                new List<string> { "Madre", "Padre" },
+                resultado,
+                "Un hijo debe tener como padres 'Madre' y 'Padre'.");
+        }
+
+        [TestMethod]
+        public void ObtenerPadresSegunParentezco_Madre_RegresaAbuelosMaternos()
+        {
+            var resultado = (List<string>)_registroPrivado!
+                .Invoke("ObtenerPadresSegunParentezco", "Madre");
+            
+
+            CollectionAssert.AreEqual(
+                new List<string> { "Abuela Materna", "Abuelo Materno" },
+                resultado,
+                "Una 'Madre' debe tener como padres los abuelos maternos.");
+        }
+
+        [TestMethod]
+        public void ObtenerNivelGeneracional_ValoresClave_CoincidenConMapaDefinido()
+        {
+            int nivelAbuela = (int)_registroPrivado!
+                .Invoke("ObtenerNivelGeneracional", "Abuela Materna");
+            int nivelMadre = (int)_registroPrivado!
+                .Invoke("ObtenerNivelGeneracional", "Madre");
+            int nivelHijo = (int)_registroPrivado!
+                .Invoke("ObtenerNivelGeneracional", "Hijo");
+            int nivelOtro = (int)_registroPrivado!
+                .Invoke("ObtenerNivelGeneracional", "Primo");
+
+            Assert.AreEqual(0, nivelAbuela, "Los abuelos deben estar en nivel 0.");
+            Assert.AreEqual(1, nivelMadre, "Madre/Padre deben estar en nivel 1.");
+            Assert.AreEqual(2, nivelHijo, "Hijo/Hija deben estar en nivel 2.");
+            Assert.AreEqual(3, nivelOtro, "Otros parentescos deben caer en el nivel 3.");
+        }
+        public void Limpiar()
+        {
+            GrafoFamiliar.Nodos.Clear();
+        }
+    }
+}
